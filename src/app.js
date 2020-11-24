964b3bca77c38029c26786dcf4054d98f95ce7e3
Pedro Castilho <pedro@castilho.in>
2020-11-24 12:50:50 -0300

    Database connection

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const { json } = require('express');
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const db = require('./config/database');
 
 app.use(express.json());
 app.use(routes);
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/instapet', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Database connected.');
+  })
+  .catch((err) => {
+    console.log('Database error: ', err);
+  });
