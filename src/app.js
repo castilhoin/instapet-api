92d040038422917108f0d4a584cf3aeb29ab50fa
Pedro Castilho <pedro@castilho.in>
2020-11-27 17:09:30 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const userRoute = require('./routes/userRoute');
+const cors = require('cors');
 const db = require('./config/database');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/users', userRoute);
 
 module.exports = app;
